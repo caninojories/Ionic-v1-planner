42714bc873324dff4b5221ef9a4659f10fd4c206
Jo-Ries Canino <canino_jories@hotmail.com>
2015-09-16 09:19:51 +0800

    adding timezone folder

---
+++ www/js/timezone/timezone.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.timezone')
+    .controller('TimezoneCtrl', TimezoneCtrl);
+
+    function TimezoneCtrl() {
+      var vm = this;
+    }
+}());
