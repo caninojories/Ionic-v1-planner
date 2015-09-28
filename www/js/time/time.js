f7f6fbaf27ff96b7567443af516ab38cbd8eaf4f
JohnMagtoto <johnmagtoto@gmail.com>
2015-09-28 10:52:55 +0800

    replaced cards of time page into inset list. Added base for swipe deletion of items in time page.

--- www/js/time/time.js
+++ www/js/time/time.js
@@ -10,26 +10,32 @@
   function TimeCtrl($rootScope, $scope, $http, $state, $timeout, PlannerService) {
       // Initialize DB
       PlannerService.initDB() ;
-      // Create design doc for key
       var vm = this ;
-      // createDesignDoc("by_urgency", function (doc) {emit(doc.timearray[0].localedate, doc);}) ;
+
 
       // $http.get('js/data/times.json').success(function(data) {
       //   $scope.timedata = data ;
       //   $scope.timecard = $state.params.timeId ;
       // });
 
-      PlannerService.getAllEvents().then(function(eventslist){
-        if (eventslist){
-          console.log(eventslist) ;
-          $scope.timedata = eventslist ;
-          $scope.timecard = $state.params.timeId ;
-        }
-      }) ;
 
       $scope.displayDetail = function(id) {
         $state.go('tabs.timedetail', {timeId: id});
       };
 
+      $scope.swipe_left = function() {
+        console.log("swiped left") ;
+      }
+
+      var init = function() {
+        PlannerService.getAllEvents().then(function(eventslist){
+          if (eventslist){
+            console.log(eventslist) ;
+            $scope.timedata = eventslist ;
+            $scope.timecard = $state.params.timeId ;
+          }
+        }) ;
+      }
+      init() ;
   }
 }()) ;
